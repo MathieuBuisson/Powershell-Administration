9c1b7aa84810ffd5a75d0ab67fee13b45d02a280
Mathieu BUISSON <matthieu.buisson@gmail.com>
2016-02-07 16:42:12 +0000

    Added Website contents and the configuration

---
+++ UpdateIISFarm/UpdateWebSite.ps1
@@ -0,0 +1,33 @@
+﻿$MyData = 
+@{
+    AllNodes = 
+    @(
+        @{
+            NodeName = "$env:computername"
+            Role     = "WebServer"
+        }
+    )
+}
+
+Configuration UpdateWebSite
+{
+    Import-DscResource -ModuleName "PSDesiredStateConfiguration"
+
+    node $AllNodes.Where{$_.Role -eq "WebServer"}.NodeName
+    {
+        File WebPageCopy
+        {
+            Ensure = "Present"  # You can also set Ensure to "Absent"
+            Type = "Directory" # Default is "File".
+            Recurse = $true
+            MatchSource = $true
+            Checksum = "SHA-1"
+            SourcePath = "\\DESKTOP-IODTS6L\SiteContents"
+            DestinationPath = "C:\inetpub\wwwroot"    
+            Force = $true # To allows overwriting existing files
+        }
+
+    }
+}
+
+UpdateWebSite -ConfigurationData $MyData -OutputPath "C:\DSCConfigs\UpdateWebSite"
