27ff02d87e77b3a42337c421818106d18bf8d673
Mathieu BUISSON <matthieu.buisson@gmail.com>
2016-02-21 23:08:59 +0000

    Allowed plain-text passwords and added a cross-node synchronization

--- UpdateIISFarm/UpdateWebSiteConfig.ps1
+++ UpdateIISFarm/UpdateWebSiteConfig.ps1
@@ -1,30 +1,57 @@
-﻿$MyData = 
-@{
+﻿$DevEnvironment = @{
     AllNodes = 
     @(
         @{
-            NodeName = "$env:computername"
-            Role     = "WebServer"
+            NodeName                   = "*"
+            PsDscAllowPlainTextPassword= $True
+            Role                       = "WebServer"
+            DestinationPath            = "C:\inetpub\wwwroot\Index.html"
+            Checksum                   = 'SHA256'
+            Force                      = $True
+            WebAppPool                 = "DefaultAppPool"
+        }
+        @{
+            NodeName = "WebServer1"
+        }
+        @{
+            NodeName = "WebServer2"
         }
     )
 }
 
 Configuration UpdateWebSite
 {
+    param(
+        [parameter(mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [PsCredential]$Credential
+    )
     Import-DscResource -ModuleName "PSDesiredStateConfiguration"
     Import-DscResource -ModuleName "cWebSiteContent"
 
-    node $AllNodes.Where{$_.Role -eq "WebServer"}.NodeName
+    Node $AllNodes.Where{$_.Role -eq "WebServer"}.NodeName
     {
         cWebSiteContent www.mat.lab
         {
-            SourcePath = "C:\Users\Mathieu\Desktop\Index.html"
-            DestinationPath = "C:\inetpub\wwwroot\Index.html"
-            Checksum = 'SHA256'
-            Force = $true
-            WebAppPool = "DefaultAppPool"
+            SourcePath = "\\DevBox\SiteContents\Index.html"
+            DestinationPath = $Node.DestinationPath
+            Checksum = $Node.Checksum
+            Force = $Node.Force
+            WebAppPool = $Node.WebAppPool
+        }
+    }
+    Node WebServer2
+    {
+        WaitForAll WaitForWebServer1
+        {
+            NodeName = "WebServer1"
+            ResourceName = "[cWebSiteContent]www.mat.lab"
+            RetryIntervalSec = 4
+            RetryCount = 5
+            PsDscRunAsCredential = $Credential
         }
     }
 }
 
-UpdateWebSite -ConfigurationData $MyData -OutputPath "C:\DSCConfigs\UpdateWebSite"
+UpdateWebSite -ConfigurationData $DevEnvironment -OutputPath "C:\DSCConfigs\UpdateWebSite" -Credential (Get-Credential)
+Start-DscConfiguration -Path "C:\DSCConfigs\UpdateWebSite" -ComputerName WebServer1,WebServer2
