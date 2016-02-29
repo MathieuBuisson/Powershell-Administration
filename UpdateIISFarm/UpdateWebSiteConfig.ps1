24c96ef0925606f642af42b0c7fdc308c43d85cd
Mathieu BUISSON <MathieuBuisson@users.noreply.github.com>
2016-02-29 14:33:51 +0000

    added -wait and -verbose to Start-DscConfiguration

--- UpdateIISFarm/UpdateWebSiteConfig.ps1
+++ UpdateIISFarm/UpdateWebSiteConfig.ps1
@@ -55,4 +55,4 @@ Configuration UpdateWebSite
 }
 
 UpdateWebSite -ConfigurationData $DevEnvironment -OutputPath "C:\DSCConfigs\UpdateWebSite" -Credential (Get-Credential)
-Start-DscConfiguration -Path "C:\DSCConfigs\UpdateWebSite" -ComputerName WebServer1,WebServer2
+Start-DscConfiguration -Path "C:\DSCConfigs\UpdateWebSite" -ComputerName WebServer1,WebServer2 -Wait -Verbose
