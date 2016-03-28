49ff8ac54301427a185cb607fe3524f5aa3bfa7f
Mathieu BUISSON <matthieu.buisson@gmail.com>
2016-03-28 21:30:28 +0100

    Added the module files for the DSC resource cRegFile

---
+++ cRegFile/SampleConfiguration/RemoteControlsConfig.ps1
@@ -0,0 +1,25 @@
+﻿$DevEnvironment = @{
+    AllNodes = 
+    @(
+        @{
+            NodeName = "Localhost"
+        }
+    )
+}
+
+Configuration RemoteControls
+{
+    param()
+    Import-DscResource -ModuleName "cRegFile"
+
+    Node $AllNodes.NodeName
+    {
+        cRegFile SupportedRemoteControls
+        {
+            key = "HKLM:\SYSTEM\CurrentControlSet\Services\HidIr\Remotes"
+            RegFilePath = "C:\DSCConfigs\Files\RemotesKey.reg"
+        }
+    }
+}
+RemoteControls -ConfigurationData $DevEnvironment -OutputPath "C:\DSCConfigs\RemoteControls\"
+Start-DscConfiguration -Path "C:\DSCConfigs\RemoteControls" -Wait -Verbose
