88849a314a4f886ca1cdb39b4c19416fd69dd7a4
Mathieu BUISSON <matthieu.buisson@gmail.com>
2016-04-23 16:22:51 +0100

    added the module for DSC resource cWindowsErrorReporting

---
+++ cWindowsErrorReporting/DSCResources/cWindowsErrorReporting/ResourceDesignerScripts/GeneratecWindowsErrorReporting.ps1
@@ -0,0 +1,3 @@
+﻿$State = New-xDscResourceProperty -Name 'State' -Type 'String' -Attribute Key -ValidateSet 'Enabled','Disabled'
+
+New-xDscResource -Name cWindowsErrorReporting -Property $State -ModuleName cWindowsErrorReporting -FriendlyName cWindowsErrorReporting -Path "C:\Git\DscModules\"
---
+++ cWindowsErrorReporting/SampleConfiguration/cWindowsErrorReportingExample.ps1
@@ -0,0 +1,24 @@
+﻿$DevEnvironment = @{
+    AllNodes = 
+    @(
+        @{
+            NodeName = "Localhost"
+        }
+    )
+}
+
+Configuration DisableWER
+{
+    param()
+
+    Import-DscResource -ModuleName "cWindowsErrorReporting"
+
+    Node $AllNodes.NodeName
+    {
+        cWindowsErrorReporting Disabled
+        {
+            State = "Disabled"
+        }
+    }
+}
+DisableWER -ConfigurationData $DevEnvironment -OutputPath "C:\DSCConfigs\DisableWER\"
